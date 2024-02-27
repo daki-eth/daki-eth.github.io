4eb28003ed4990890f716e82fe280e778625a297
daki-eth <damianlucicbusiness@gmail.com>
2024-02-26 21:28:48 -0500

    lab5update

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-// main.js
-
+// Existing functions remain unchanged
 function promptName() {
     var name = prompt("Please enter your name:");
     if (name != null && name != "") {
@@ -25,11 +24,35 @@ function unhighlight(element) {
     element.style.backgroundColor = "";
 }
 
+function handleEducationClick() {
+    alert("You clicked on the Education section!");
+}
+
+function handleContactClick() {
+    alert("You clicked on the Contact Information section!");
+}
+
+// Updated toggleDropdown function
 function toggleDropdown(element) {
     var dropdownContent = element.nextElementSibling;
     if (dropdownContent.style.display === "block") {
         dropdownContent.style.display = "none";
     } else {
+        // Close all open dropdowns before opening the new one
+        var allDropdowns = document.querySelectorAll('.dropdown-content');
+        for (var i = 0; i < allDropdowns.length; i++) {
+            allDropdowns[i].style.display = 'none';
+        }
         dropdownContent.style.display = "block";
     }
 }
+
+// Close dropdowns when clicking outside
+document.addEventListener('click', function(event) {
+    var allDropdowns = document.querySelectorAll('.dropdown-content');
+    for (var i = 0; i < allDropdowns.length; i++) {
+        if (!allDropdowns[i].contains(event.target)) {
+            allDropdowns[i].style.display = 'none';
+        }
+    }
+}, true);
